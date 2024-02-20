d92e65de3d89e22c3603c6992b22af7ca50c7b63
Garrek Stemo <8449000+garrekstemo@users.noreply.github.com>
2024-02-20 16:58:31 +0900

    finish notebook example

--- notebook/2024_02_20.jl
+++ notebook/2024_02_20.jl
@@ -4,8 +4,11 @@ using GLMakie
 
 dir = "data/raw/2024_02_20"
 
+##
+# Data set 1
+
 # Load data and fit
-df = CSV.read(joinpath(dir, "2024_02_20.csv"), DataFrame)
+df = CSV.read(joinpath(dir, "data1.csv"), DataFrame)
 
 p0 = [1.0, 1.0]
 fit = curve_fit(Experiment.linear, df.x, df.y, p0)
@@ -17,7 +20,28 @@ DataInspector()  # information when mouse hovers over plot
 
 ax = Axis(fig[1, 1], xlabel = "Frequency (cm⁻¹)", ylabel = "Intensity (a. u.)")
 scatter!(df.x, df.y, label = "raw")
-lines!(xfit, Experiment.linear(xfit, fit.param), color = :black, label = "fit")
+lines!(xfit, linear(xfit, fit.param), color = :black, label = "fit")
+
+axislegend(ax)
+fig
+
+##
+# Data set 2
+
+# Load data and fit
+df = CSV.read(joinpath(dir, "data2.csv"), DataFrame)
+
+p0 = [1.0, 1.0, 0.0]
+fit = curve_fit(Experiment.expdecay, df.x, df.y, p0)
+xfit = range(df.x[1], df.x[end], length=100)
+
+# Plot
+fig = Figure()
+DataInspector()  # information when mouse hovers over plot
+
+ax = Axis(fig[1, 1], xlabel = "Frequency (cm⁻¹)", ylabel = "Intensity (a. u.)")
+scatter!(df.x, df.y, label = "raw")
+lines!(xfit, expdecay(xfit, fit.param), color = :black, label = "fit")
 
 axislegend(ax)
 fig
--- src/generate_data.jl
+++ src/generate_data.jl
@@ -16,17 +16,17 @@ function generate_expdecay()
     return x, y
 end
 
-data_path = "data/raw"
+data_path = "data/raw/2024_02_20/"
 
 # Write line
-fname = "2024_02_20/2024_02_20.csv"
+fname = "data1.csv"
 f = joinpath(data_path, fname)
 x, y = generate_line()
 df = DataFrame(x=x, y=y)
 CSV.write(f, df)
 
 # Write exponential decay
-fname = "2024_02_21/2024_02_21.csv"
+fname = "data2.csv"
 f = joinpath(data_path, fname)
 x, y = generate_expdecay()
 df = DataFrame(x=x, y=y)
